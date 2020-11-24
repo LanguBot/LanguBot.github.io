e7b7a5784162c32911f644078a2902e1f2bcbc60
Zemingd <dongzm_0405@yahoo.co.jp>
2020-11-24 11:41:23 +0900

    langubot_03

--- sw.js
+++ sw.js
@@ -31,9 +31,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "langubot.com"
+
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
